2dc6f0bd9583a4fb120d9e68b235e64d7cebf1bc
MrSebi06 <mihneasebastian06@gmail.com>
2025-07-24 21:31:29 +0200

    Update visuals

--- Assets/VelocityAwarePoke.cs
+++ Assets/VelocityAwarePoke.cs
@@ -4,11 +4,11 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class VelocityAwarePoke : MonoBehaviour
 {
-    XRPokeInteractor pokeInteractor;
-    private InputDevice _device;
+    private XRPokeInteractor pokeInteractor;
+    private InputDevice device;
 
     [Header("Velocity Settings")]
-    [SerializeField] private float minPokeDepth = 0.01f;
+    [SerializeField] private float minPokeDepth = 0.003f;
     [SerializeField] private float maxPokeDepth = 0.05f;
     [SerializeField] private float velocityThreshold = 2f;
 
@@ -18,22 +18,20 @@ public class VelocityAwarePoke : MonoBehaviour
 
         if (transform.name.Contains("Left") || transform.parent.name.Contains("Left"))
         {
-            _device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+            device = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
         }
         else
         {
-            _device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+            device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
         }
     }
 
     void Update()
     {
-        Vector3 velocity;
-        if (_device.TryGetFeatureValue(CommonUsages.deviceVelocity, out velocity))
-        {
-            float speed = velocity.magnitude;
-            float normalizedSpeed = Mathf.Clamp01(speed / velocityThreshold);
-            pokeInteractor.pokeDepth = Mathf.Lerp(minPokeDepth, maxPokeDepth, normalizedSpeed);
-        }
+        if (!device.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity)) return;
+
+        float speed = velocity.magnitude;
+        float normalizedSpeed = Mathf.Clamp01(speed / velocityThreshold);
+        pokeInteractor.pokeDepth = Mathf.Lerp(minPokeDepth, maxPokeDepth, normalizedSpeed);
     }
 }
